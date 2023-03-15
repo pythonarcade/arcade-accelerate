76926e27bdcd683a680231806049030c8d49236a
Darren Eberly <darren.eberly@gmail.com>
2023-03-15 17:41:06 -0400

    Initial messing with custom classes for hit boxes

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,74 @@
 use pyo3::prelude::*;
 
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+trait BaseHitBox {}
+
+#[pyclass]
+struct HitBox {
+    points: Vec<(f32, f32)>,
+}
+
+#[pymethods]
+impl HitBox {
+    #[new]
+    fn py_new(points: Vec<(f32, f32)>) -> Self {
+        HitBox { points: points }
+    }
+
+    fn create_adjustable(
+        &self,
+        position: (f32, f32),
+        angle: f32,
+        scale: (f32, f32),
+    ) -> AdjustableHitBox {
+        let adjustable: AdjustableHitBox = AdjustableHitBox {
+            hitbox: *self,
+            position: position,
+            angle: angle,
+            scale: scale,
+        };
+        adjustable
+    }
+
+    fn get_adjusted_points(&self) -> &Vec<(f32, f32)> {
+        &self.points
+    }
+}
+
+#[pyclass]
+struct AdjustableHitBox {
+    hitbox: HitBox,
+    position: (f32, f32),
+    angle: f32,
+    scale: (f32, f32),
+}
+
+#[pymethods]
+impl AdjustableHitBox {
+    fn get_adjusted_points(&self) -> Vec<(f32, f32)> {
+        let mut new_points: Vec<(f32, f32)> = Vec::with_capacity(self.hitbox.points.len());
+
+        let rad = &self.angle.to_radians();
+        let rad_cos = rad.cos();
+        let rad_sin = rad.sin();
+        for point in self.hitbox.points.iter() {
+            new_points.push(self.adjust_point(point, rad_cos, rad_sin));
+        }
+        new_points
+    }
+
+    fn adjust_point(&self, point: &(f32, f32), cos: f32, sin: f32) -> (f32, f32) {
+        let (mut x, mut y) = point;
+        x = ((x * cos - y * sin) * self.scale.0) + self.position.0;
+        y = ((x * sin + y * cos) * self.scale.0) + self.position.0;
+        (x, y)
+    }
+}
+
 #[pyfunction]
 fn rotate_point(point: (f32, f32), center: (f32, f32), angle: f32) -> (f32, f32) {
     let (x, y) = point;
