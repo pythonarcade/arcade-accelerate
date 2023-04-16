078b91d40deea00873cb27f0a62c88622d21562c
Darren Eberly <darren@eber.ly>
2023-04-16 00:29:02 -0400

    Support rotatable hit boxes in check_for_collision_with_list

--- src/sprite_list.rs
+++ src/sprite_list.rs
@@ -10,22 +10,67 @@ pub fn check_for_collision_with_list(
 ) -> Vec<PyObject> {
     let mut final_sprites: Vec<PyObject> = Vec::new();
 
+    let mut hitbox1: Option<HitBox> = None;
+    let mut hitbox2: Option<PyRef<'_, RotatableHitBox>> = None;
+
+    let cls: &str = sprite
+        .getattr("_hit_box")
+        .unwrap()
+        .get_type()
+        .name()
+        .unwrap();
+
+    match cls {
+        "HitBox" => {
+            hitbox1 = sprite.getattr("_hit_box").unwrap().extract().unwrap();
+        }
+        "RotatableHitBox" => {
+            hitbox2 = sprite.getattr("_hit_box").unwrap().extract().unwrap();
+        }
+        _ => panic!(),
+    }
+
+    let main_points: Vec<(f32, f32)>;
+
+    if hitbox1.is_some() {
+        main_points = hitbox1.unwrap().get_adjusted_points_native();
+    } else {
+        main_points = RotatableHitBox::get_adjusted_points(hitbox2.unwrap());
+    }
+
     let sprite_list_list = sprite_list.getattr("sprite_list").unwrap();
     let sprites_to_check: Vec<PyObject> = sprite_list_list.extract().unwrap();
 
     for sprite2 in sprites_to_check.iter() {
-        let hitbox1: HitBox = sprite.getattr("_hit_box").unwrap().extract().unwrap();
-        let hitbox2: HitBox = sprite2
-            .getattr(py, "_hit_box")
+        let other_sprite: &PyAny = sprite2.as_ref(py);
+        let mut other_hitbox1: Option<HitBox> = None;
+        let mut other_hitbox2: Option<PyRef<'_, RotatableHitBox>> = None;
+        let other_cls: &str = other_sprite
+            .getattr("_hit_box")
             .unwrap()
-            .extract(py)
+            .get_type()
+            .name()
             .unwrap();
 
-        let check_2 = are_polygons_intersecting(
-            hitbox1.get_adjusted_points_native().to_vec(),
-            //HitBox::get_adjusted_points_native(hitbox1).to_vec(),
-            HitBox::get_adjusted_points_native(hitbox2).to_vec(),
-        );
+        match other_cls {
+            "HitBox" => {
+                other_hitbox1 = other_sprite.getattr("_hit_box").unwrap().extract().unwrap();
+            }
+            "RotatableHitBox" => {
+                other_hitbox2 = other_sprite.getattr("_hit_box").unwrap().extract().unwrap();
+            }
+            _ => panic!(),
+        }
+
+        let other_points: Vec<(f32, f32)>;
+
+        if other_hitbox1.is_some() {
+            other_points = other_hitbox1.unwrap().get_adjusted_points_native();
+        } else {
+            other_points = RotatableHitBox::get_adjusted_points(other_hitbox2.unwrap());
+        }
+
+        let check_2 = are_polygons_intersecting(main_points.to_vec(), other_points);
 
         if check_2 {
             final_sprites.push(sprite2.to_object(py));
