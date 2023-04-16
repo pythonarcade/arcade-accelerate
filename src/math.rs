363e48e6479c4389b59f1922f75b7229b816e35a
Yuvakiran <yuki16616@gmail.com>
2023-04-16 13:44:36 +0530

    added lerp,lerp_angle and lerp_vec

--- src/math.rs
+++ src/math.rs
@@ -33,3 +33,30 @@ pub fn clamp(value: f32, min: f32, max: f32) -> f32 {
         value
     }
 }
+
+#[pyfunction]
+pub fn lerp(v1: f32, v2: f32, u: f32) -> f32 {
+    v1 + ((v2 - v1) * u)
+}
+
+#[pyfunction]
+pub fn lerp_vec(v1: (f32, f32), v2: (f32, f32), u: f32) -> (f32, f32) {
+    (lerp(v1.0, v2.0, u), lerp(v1.1, v2.1, u))
+}
+
+#[pyfunction]
+pub fn lerp_angle(start_angle: f32, end_angle: f32, u: f32) -> f32 {
+    
+    let mut temp_start = start_angle % 360.0;
+    let mut temp_end = end_angle % 360.0;
+
+    while temp_start - temp_end > 180.0 {
+        temp_end += 360.0;
+    }
+
+    while temp_start - temp_end < - 180.0 {
+        temp_end -= 360.0;
+    }
+
+    lerp(temp_start, temp_end, u) % 360.0
+}
